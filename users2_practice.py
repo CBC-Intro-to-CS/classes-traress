59f0822a8f9a07ed92bb1d86864dd5ef12451514
Sam <traress28@cbchs.org>
2024-11-07 09:37:46 -0600

    updated users assignment

---
+++ users2_practice.py
@@ -0,0 +1,56 @@
+class Uber:
+    def __init__(self, first_name, last_name, gender, age):
+        #initialize first name and last name
+        self.first_name = first_name
+        self.last_name = last_name 
+        self.age = age 
+        self.gender = gender
+        
+    def describe_user(self):
+        print(f"{self.first_name} {self.last_name} is a {self.gender} and is {self.age} years old.")
+
+    def greet_user(self):
+        print(f"Hello, {self.first_name} {self.last_name} welcome!")
+
+user1 = Uber('Sam', 'Trares', 'male', 14)
+
+user1.describe_user()
+user1.greet_user() 
+
+class Uber:
+    def __init__(self, first_name, last_name, gender, age):
+        #initialize first name and last name
+        self.first_name = first_name
+        self.last_name = last_name 
+        self.age = age 
+        self.gender = gender
+        
+    def describe_user(self):
+        print(f"{self.first_name} {self.last_name} is a {self.gender} and is {self.age} years old.")
+
+    def greet_user(self):
+        print(f"Hello, {self.first_name} {self.last_name} welcome!")
+
+user1 = Uber('Connor', 'Smith', 'male', 97)
+
+user1.describe_user()
+user1.greet_user()
+
+class Uber:
+    def __init__(self, first_name, last_name, gender, age):
+        #initialize first name and last name
+        self.first_name = first_name
+        self.last_name = last_name 
+        self.age = age 
+        self.gender = gender
+        
+    def describe_user(self):
+        print(f"{self.first_name} {self.last_name} is a {self.gender} and is {self.age} years old.")
+
+    def greet_user(self):
+        print(f"Hello, {self.first_name} {self.last_name} welcome!")
+
+user1 = Uber('John', 'Phillips', 'male', 36)
+
+user1.describe_user()
+user1.greet_user()
